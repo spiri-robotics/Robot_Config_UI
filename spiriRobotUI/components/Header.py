c24027c4351c70758596a084fe71e2dc0c77604a
AxisShine <alicejsedgwick2004@gmail.com>
2025-06-19 20:13:03 +0000

    colour updates

--- spiriRobotUI/components/Header.py
+++ spiriRobotUI/components/Header.py
@@ -4,6 +4,6 @@ def header() -> None:
     """Render the header."""
     with ui.header().classes('bg-white text-black flex justify-between items-center px-4'):
         ui.space()
-        ui.button('', icon='more_vert').classes('text-sm text-gray-600')
-        ui.button('', icon='account_circle').classes('text-sm text-black ml-auto')
+        ui.button('', icon='more_vert').classes('bg-transparent shawdow-none text-sm text-black')
+        ui.button('', icon='account_circle').classes('bg-transparent shadow-none text-sm text-black ml-auto')
         
