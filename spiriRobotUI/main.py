cffd85cf3bbab28b9acb50bd5038d68fe6d962e3
pendrith <Sidney.scott.wood@gmail.com>
2025-06-23 12:21:06 -0400

    Replaced the main.py with the main main.py

--- spiriRobotUI/main.py
+++ spiriRobotUI/main.py
@@ -1,18 +1,15 @@
-from pathlib import Path
-
 from nicegui import ui
-
-from spiriRobotUI.pages.NetworkSettingsPage import network_ui as network_settings_page
+from pathlib import Path
 from spiriRobotUI.pages.PluginsPage import main_ui as plugins_page
+from spiriRobotUI.pages.NetworkSettingsPage import network_ui as network_settings_page
 from spiriRobotUI.pages.SystemMonitorPage import system_ui as system_monitor_page
 
 favicon = "spiriRobotUI/icons/spiri_drone_ui_logo.svg"
 
-ui.run(
-    title="Robot Config UI",
-    favicon=favicon,
-    port=8080,
-    show=True,
-    uvicorn_reload_dirs=str(Path(__file__).parent.resolve()),
-    reload=True,
-)
+ui.run(title='Robot Config UI',
+        favicon=favicon,
+        port=8080,
+        show=True,
+        uvicorn_reload_dirs=str(Path(__file__).parent.resolve()),
+        reload=True,
+       )
