3592310a4531617fec5df0240439445fc6a9676b
pendrith <Sidney.scott.wood@gmail.com>
2025-06-23 15:55:38 +0000

    reorganized main

--- spiriRobotUI/main.py
+++ spiriRobotUI/main.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 
+from nicegui import ui
+
 from spiriRobotUI.pages.NetworkSettingsPage import network_ui as network_settings_page
 from spiriRobotUI.pages.PluginsPage import main_ui as plugins_page
 from spiriRobotUI.pages.SystemMonitorPage import system_ui as system_monitor_page
 
 favicon = "spiriRobotUI/icons/spiri_drone_ui_logo.svg"
-
-from nicegui import ui
-
-from spiriRobotUI.components.Header import header
-from spiriRobotUI.components.Sidebar import sidebar
 
 ui.run(
     title="Robot Config UI",
